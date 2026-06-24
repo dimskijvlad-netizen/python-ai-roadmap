398f3d747ca9cee38296d48cce56f6eea5316d71
Vladyslav <dimskijvlad667@gmail.com>
2026-06-24 13:49:01 +0300

    Add JSON Exporter project

--- lesson06/json_exporter.py
+++ lesson06/json_exporter.py
@@ -1,4 +1,19 @@
 import json
-user = '{"name":"Vlad","python":"Beginner"}'
-user1 = json.loads(user)
-print(user1["python"])
+def get_user_data():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    english = input("Enter your English level: ")
+    python_level = input("Enter your Python level: ")
+    return name, age, english, python_level
+def save_in_dictionary(name, age, english, python_level):
+    dictionary = {
+        "name": name,
+        "age": age,
+        "english": english,
+        "python_level": python_level
+    }
+    return dictionary
+def convert_to_json(dictionary):
+    json_data = json.dumps(dictionary,indent=4)
+    return json_data
+print(convert_to_json(save_in_dictionary(*get_user_data())))
