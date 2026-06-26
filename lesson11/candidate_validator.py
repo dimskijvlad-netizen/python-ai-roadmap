f2dfaa14cca9414cb5bfa5235f92aca3933343b8
Vladyslav <dimskijvlad667@gmail.com>
2026-06-26 13:03:13 +0300

    Add exception handling with try-except

---
+++ lesson11/candidate_validator.py
@@ -0,0 +1,17 @@
+def get_candidate_data():
+    try:
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        english = input("Enter your English level: ")
+        return name, age, english
+    except ValueError: 
+        print("Age must be a number ")
+        return None, None, None
+def print_candidate_validation(name, age, english):
+    if name != None: 
+        print("\nCandidate validated successfully")
+        print(f"\nName: {name}")
+        print(f"Age: {age}")
+        print(f"English: {english}")
+    else: print("Candidate unvalidated")
+print_candidate_validation(*get_candidate_data())
